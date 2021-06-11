fee1ff6dda83b91d4b96798bd99ed43d2b8832f3
Yuliya <jknoroz@gmail.com>
2021-06-11 13:06:14 +0300

    2

--- color-switch.js
+++ color-switch.js
@@ -13,9 +13,34 @@ const randomIntegerFromInterval = (min, max) => {
 
 const startBtn = document.querySelector('button[data-action="start"]');
 const stopBtn = document.querySelector('button[data-action="stop"]');
+const DELAY = 1000;
 
-startBtn.addEventListener("click", onStrartClick);
+// startBtn.addEventListener("click", onStartClick);
+// stopBtn.addEventListener("click", onStopClick);
 
-stopBtn.addEventListener("click", onStopClick);
+// function onStartClick() {
+//   console.log("start");
+//   bodyColor(colors[randomIntegerFromInterval(0, colors.length - 1)]);
+// }
 
-document.body.style.backgroundColor = colors[1];
+// const intervalId = setInterval(onStartClick, DELAY);
+
+// function onStopClick() {
+//   console.log("stop");
+//   clearInterval(intervalId);
+// }
+
+function bodyColor(color) {
+  document.body.style.backgroundColor = color;
+}
+
+startBtn.addEventListener("click", () => {
+  const intervalId = setInterval(() => {
+    console.log("start");
+    bodyColor(colors[randomIntegerFromInterval(0, colors.length - 1)]);
+  }, DELAY);
+});
+
+stopBtn.addEventListener("click", () => {
+  clearTimeout(intervalId);
+});
